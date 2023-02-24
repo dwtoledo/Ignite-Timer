9cde6cc983a4a62072b87e13818e16e0d36e8367
Douglas Toledo <dwtoledo@outlook.com>
2023-02-24 10:15:27 -0300

    Update DefaultTheme Interface extending DefaultThemeType Interface

---
+++ project-02-ignite/src/@types/styled.d.ts
@@ -0,0 +1,8 @@
+import 'styled-components'
+import { defaultTheme } from '../assets/styles/themes/default'
+
+type DefaultThemeType = typeof defaultTheme;
+
+declare module 'styled-components' {
+    export interface DefaultTheme extends DefaultThemeType {}
+}
