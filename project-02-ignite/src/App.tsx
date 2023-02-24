f68c289f78cd6685ff8d63e0c8c14827bd5dea5e
Douglas Toledo <dwtoledo@outlook.com>
2023-02-24 10:27:41 -0300

    Move reset.css into Styled Components Global Style

--- project-02-ignite/src/App.tsx
+++ project-02-ignite/src/App.tsx
@@ -3,18 +3,19 @@ import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './assets/styles/themes/default'
 
-import './reset.css'
 import { GlobalStyle } from './assets/styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+
       <Button variant='error'/>
       <Button variant='info'/>
       <Button variant='success'/>
       <Button variant='warning'/>
       <Button />
-      <GlobalStyle/>
+      
     </ThemeProvider>
   )
 }
