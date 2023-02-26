b56f6640feec62d3f3325a662ac072f179fabaa0
Douglas Toledo <dwtoledo@outlook.com>
2023-02-26 10:24:27 -0300

    Implement React Router DOM
    Route to Home Page is default
    Route do History Page is /history
    Implement Default Layout to Reuse Header Component

--- project-02-ignite/src/App.tsx
+++ project-02-ignite/src/App.tsx
@@ -1,12 +1,15 @@
 import { ThemeProvider } from 'styled-components'
-import { defaultTheme } from './assets/styles/themes/default'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from './routes'
 
+import { defaultTheme } from './assets/styles/themes/default'
 import { GlobalStyle } from './assets/styles/global'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <RouterProvider router={routes} />
     </ThemeProvider>
   )
 }
---
+++ project-02-ignite/src/routes.tsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { DefaultLayout } from './layouts/deafultLayout'
+import { History } from './pages/History'
+import { Home } from './pages/Home'
+
+export const routes = createBrowserRouter([
+  {
+    path: '/',
+    element: <DefaultLayout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/history',
+        element: <History />,
+      },
+    ],
+  },
+])
