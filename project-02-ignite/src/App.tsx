121bc28077f8024aacaf3925337269242e072f92
Douglas Toledo <dwtoledo@outlook.com>
2023-02-26 10:00:07 -0300

    Remove Button Component used just for tests purpose

--- project-02-ignite/src/App.tsx
+++ project-02-ignite/src/App.tsx
@@ -1,4 +1,3 @@
-import { Button } from './components/Button'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './assets/styles/themes/default'
 
@@ -8,13 +7,6 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-
-      <Button variant="red" />
-      <Button variant="red-dark" />
-      <Button variant="green" />
-      <Button variant="green-dark" />
-      <Button variant="green-light" />
-      <Button />
     </ThemeProvider>
   )
 }
