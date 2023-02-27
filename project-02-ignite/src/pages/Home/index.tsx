a58ae8c96754923918a8e96c20434a5dbc52570a
Douglas Toledo <dwtoledo@outlook.com>
2023-02-27 20:53:05 -0300

    Implement and style Home Component

--- project-02-ignite/src/pages/Home/index.tsx
+++ project-02-ignite/src/pages/Home/index.tsx
@@ -1,3 +1,53 @@
+import { Play } from 'phosphor-react'
+import {
+  MinutesAmountInput,
+  PomodoroCountdownContainer,
+  PomodoroCountdownDivider,
+  PomodoroFormContainer,
+  PomodoroInfoContainer,
+  StartButtonContainer,
+  TaskNameInput,
+} from './styles'
+
 export function Home() {
-  return <h1>Home</h1>
+  return (
+    <PomodoroFormContainer action="">
+      <PomodoroInfoContainer>
+        <label htmlFor="task-name">I will work on</label>
+        <TaskNameInput
+          type="text"
+          id="task-name"
+          list="task-suggestions"
+          placeholder="task name"
+        />
+
+        <datalist id="task-suggestions">
+          <option value="Task 1" />
+          <option value="Task 2" />
+          <option value="Task 3" />
+        </datalist>
+
+        <label htmlFor="minutes-amount">for</label>
+        <MinutesAmountInput
+          type="number"
+          id="minutes-amount"
+          placeholder="00"
+        />
+        <span>minutes.</span>
+      </PomodoroInfoContainer>
+
+      <PomodoroCountdownContainer>
+        <span>0</span>
+        <span>0</span>
+        <PomodoroCountdownDivider>:</PomodoroCountdownDivider>
+        <span>0</span>
+        <span>0</span>
+      </PomodoroCountdownContainer>
+
+      <StartButtonContainer type="submit">
+        <Play size={24} />
+        Start
+      </StartButtonContainer>
+    </PomodoroFormContainer>
+  )
 }
---
+++ project-02-ignite/src/pages/Home/styles.ts
@@ -0,0 +1,112 @@
+import styled from 'styled-components'
+
+export const PomodoroFormContainer = styled.form`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  gap: 3.5rem;
+`
+
+export const PomodoroInfoContainer = styled.div`
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  justify-content: center;
+
+  label,
+  span {
+    font-size: 1.125rem;
+    font-weight: 700;
+  }
+`
+
+const DefaultInput = styled.input`
+  background-color: transparent;
+  border: none;
+  border-bottom: 2px solid ${(props) => props.theme['gray-400']};
+  color: ${(props) => props.theme['gray-700']};
+  font-size: 1.125rem;
+  font-weight: 700;
+  line-height: 1;
+  padding: 0.5rem;
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-400']};
+    text-align: center;
+  }
+`
+
+export const TaskNameInput = styled(DefaultInput)`
+  flex: 1;
+`
+
+export const MinutesAmountInput = styled(DefaultInput)`
+  width: 74px;
+`
+
+export const StartButtonContainer = styled.button`
+  cursor: pointer;
+  align-items: center;
+  background-color: ${(props) => props.theme.green};
+  border: none;
+  border-radius: 8px;
+  box-shadow: none;
+  color: ${(props) => props.theme['gray-700']};
+  display: flex;
+  font-weight: 700;
+  gap: 8px;
+  justify-content: center;
+  max-width: 250px;
+  padding: 1.25rem;
+  width: 100%;
+
+  &:not(:disabled):hover,
+  &:not(:disabled):active {
+    background-color: ${(props) => props.theme['green-dark']};
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.7;
+  }
+`
+
+export const PomodoroCountdownDivider = styled.div`
+  color: ${(props) => props.theme.green};
+  font-family: 'Ubuntu Mono', monospace;
+  font-size: 10rem;
+  font-weight: 700;
+  line-height: 1;
+`
+
+export const PomodoroCountdownContainer = styled.div`
+  align-items: center;
+  display: flex;
+  gap: 1rem;
+  justify-content: center;
+
+  span {
+    background-color: ${(props) => props.theme['gray-300']};
+    border-radius: 8px;
+    font-family: 'Ubuntu Mono', monospace;
+    font-size: 10rem;
+    font-weight: 700;
+    line-height: 1;
+    padding: 1rem;
+  }
+
+  @media (max-width: 715px) {
+    span,
+    div {
+      font-size: 5rem;
+    }
+  }
+
+  @media (max-width: 500px) {
+    span,
+    div {
+      font-size: 2.5rem;
+    }
+  }
+`
